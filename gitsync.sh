3d452141b048f7278ec89a5e51ec44b768b8e534
Perry Werneck <perry.werneck@gmail.com>
2018-12-03 16:37:35 -0200

    Adding syncronization script.

---
+++ gitsync.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# https://help.github.com/articles/syncing-a-fork/
+#
+# https://help.github.com/articles/configuring-a-remote-for-a-fork/
+#
+# https://www.opentechguides.com/how-to/article/git/177/git-sync-repos.html
+#
+# Setup:
+#
+# git remote add github https://github.com/PerryWerneck/lib3270.git
+#
+#
+
+git fetch origin
+git checkout master
+git merge origin/master
+
+for repo in $(git remote -v | grep -v origin | grep "(push)" | awk '{print $1}')
+do
+	echo "Updating ${repo} ..."
+	git push ${repo}
+done
+
