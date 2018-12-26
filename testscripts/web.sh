7632ef36905f569cb35034229595423ee7a13b3b
Perry Werneck <perry.werneck@gmail.com>
2018-12-26 11:20:39 -0200

    Adding "broadway" test script.

---
+++ testscripts/web.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# https://developer.gnome.org/gtk3/stable/gtk-broadway.html
+#
+# zypper in gtk3-immodule-broadway
+#
+
+export GDK_BACKEND=broadway
+# export BROADWAY_DISPLAY=:5
+.bin/Debug/libv3270
+
