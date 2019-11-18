05e56a0d55e220d24d85437286c51719ad357b97
Perry Werneck <perry.werneck@gmail.com>
2019-11-18 16:49:06 -0300

    Fixing color schemes in debug version.

--- src/dialogs/colorscheme.c
+++ src/dialogs/colorscheme.c
@@ -267,11 +267,7 @@
 	GtkWidget		* widget	= GTK_WIDGET(g_object_new(GTK_TYPE_V3270_COLOR_SCHEME, NULL));
 	GtkTreeModel	* model		= gtk_combo_box_get_model(GTK_COMBO_BOX(widget));
 
-#ifdef DEBUG
-	g_autofree gchar * filename = g_build_filename("conf","colors.conf",NULL);
-#else
 	lib3270_autoptr(char) filename = lib3270_build_data_filename("colors.conf",NULL);
-#endif // DEBUG
 
  	if(!g_file_test(filename,G_FILE_TEST_IS_REGULAR))
 	{
