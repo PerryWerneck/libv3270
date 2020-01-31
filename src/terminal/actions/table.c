9e9bc5811cf960ea7021afa9e40cf29f95bb387d
Perry Werneck <perry.werneck@gmail.com>
2020-01-31 11:37:57 -0300

    The message for screen paste was too large for the dialog box. Fixing it.

--- src/terminal/actions/table.c
+++ src/terminal/actions/table.c
@@ -198,7 +198,8 @@
  		.group = LIB3270_ACTION_GROUP_ONLINE,
 		.name = "paste-screen",
 		.label = N_("Paste formatted screen"),
-		.summary = N_("Search clipboard for a similar screen, if found paste unprotected fields and restore cursor position"),
+		.summary = N_("Paste similar screen from clipboard"),
+		.description = N_("Search clipboard for a similar screen, if found paste unprotected fields and restore cursor position"),
 		.key = 'v',
 		.mods = GDK_SHIFT_MASK|GDK_CONTROL_MASK|GDK_ALT_MASK,
  		.activate = fire_paste_accelerator
